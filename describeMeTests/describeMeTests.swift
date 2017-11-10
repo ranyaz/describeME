4d33ea9616248d5eed2b65ed06dce74d480d83fc
Rany <azevedo.rany@gmail.com>
2017-11-10 15:19:40 +0100

    Initial Commit

---
+++ describeMeTests/describeMeTests.swift
@@ -0,0 +1,36 @@
+//
+//  describeMeTests.swift
+//  describeMeTests
+//
+//  Created by Rany Azevedo on 10/11/2017.
+//  Copyright © 2017 Rany Azevedo. All rights reserved.
+//
+
+import XCTest
+@testable import describeMe
+
+class describeMeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
